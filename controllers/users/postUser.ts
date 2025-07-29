b014b4af05d5dfc4a116ca349a9d0d6cf625e0c1
RK Anik <30260735+rkanik@users.noreply.github.com>
2025-07-29 12:57:04 +0600

    Added crypto hash

--- controllers/users/postUser.ts
+++ controllers/users/postUser.ts
@@ -1,36 +1,36 @@
-import { Context } from "oak";
-import { TZPostUser, zPostUser } from "./zod.ts";
-import { z } from "zod";
-import { getId } from "@/utils/getId.ts";
-import { kv } from "@/kv.ts";
-// import { hash } from "bcrypt";
-import { TUser } from "./types.ts";
+import { Context } from 'oak'
+import { TZPostUser, zPostUser } from './zod.ts'
+import { z } from 'zod'
+import { getId } from '@/utils/getId.ts'
+import { kv } from '@/kv.ts'
+import { TUser } from './types.ts'
+import { hashWithSHA256 } from '@/utils/hashWithSHA256.ts'
 
 export const postUser = async (ctx: Context) => {
-  const body = await ctx.request.body.json();
-  const parsed = zPostUser.safeParse(body);
-  if (!parsed.success) {
-    ctx.response.status = 422;
-    ctx.response.body = z.treeifyError(parsed.error);
-    return;
-  }
+	const body = await ctx.request.body.json()
+	const parsed = zPostUser.safeParse(body)
+	if (!parsed.success) {
+		ctx.response.status = 422
+		ctx.response.body = z.treeifyError(parsed.error)
+		return
+	}
 
-  const id = await getId();
-  const data = parsed.data as TZPostUser;
-  //   const password = await hash(data.password);
+	const id = await getId()
+	const data = parsed.data as TZPostUser
+	const password = await hashWithSHA256(data.password)
 
-  const user: TUser = {
-    id,
-    ...data,
-    //  password,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-  await kv.set(["users", id], user);
+	const user: TUser = {
+		id,
+		...data,
+		password,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	}
+	await kv.set(['users', id], user)
 
-  ctx.response.status = 201;
-  ctx.response.body = {
-    message: "User created successfully",
-    user,
-  };
-};
+	ctx.response.status = 201
+	ctx.response.body = {
+		message: 'User created successfully',
+		user,
+	}
+}
---
+++ utils/hashWithSHA256.ts
@@ -0,0 +1,8 @@
+export async function hashWithSHA256(password: string): Promise<string> {
+	const encoder = new TextEncoder()
+	const data = encoder.encode(password)
+	const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+	return Array.from(new Uint8Array(hashBuffer))
+		.map((b) => b.toString(16).padStart(2, '0'))
+		.join('')
+}
